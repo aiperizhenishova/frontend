9e8897d96f80cb952a366566a0501e352d9c1c65
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2026-01-11 22:34:52 +0600

    created app weather

--- weather-app/script.js
+++ weather-app/script.js
@@ -3,45 +3,33 @@
 
 const temperatureField = document.querySelector(".temperature");
 const locationField = document.querySelector(".time-location p");
-const dateandTimeField = document.querySelector(".time-location span");
-const contionField = document.querySelector(".condition p");
-const searchField = document.querySelector(".search-area");
+const dateandTimeField = document.getElementById(".time-location span");
+const conditionField = document.querySelector(".condition p");
 const form = document.querySelector(".form");
+const searchField = document.querySelector(".search-area")
 
 
 form.addEventListener('submit', searchForLocation)
 
-
 let target = 'Karakol'
 
-const fetchResults = async (targetLocation)=> {
+
+const fetchResults = async (targetLocation)=>{
     let url = `http://api.weatherapi.com/v1/current.json?key=f2c2286185c44da9b33115026260901&q=${targetLocation}&aqi=no`
 
-    const res = await fetch(url)
-    const data = await res.json()
+    // fetch(url) — это функция и она не возвращает сразу данные, а возвращает Promise — это как обещание:
+    // «Я принесу данные позже, когда сервер ответит» 
+    let res = await fetch(url)
+    let data = await res.json()
 
     console.log(data)
 
-    let locationName = data.location.name 
+    let locationName = data.location.name
     let time = data.location.localtime
     let temp = data.current.temp_c
     let condition = data.current.condition.text
 
-    updateDetails(temp, locationName, time, condition)
-}
-
-
-function updateDetails(temp, locationName, time, condition){
-
-
-    let splitDate = time.split(' ')[0]
-    let splitTime = time.split(' ')[1]
-    let currentDay = getDayName(new Date(splitDate).getDay()) 
-
-    temperatureField.innerText = temp
-    locationField.innerText = locationName
-    dateandTimeField.innerText = `${splitDate} ${currentDay} ${splitTime} `
-    contionField.innerText = condition
+    updateDetails(locationName, time, temp, condition)
 }
 
 
@@ -50,29 +38,34 @@ function searchForLocation(e){
 
     target = searchField.value 
 
-    fetchResults(target)
+    fetchResults(target) 
 }
-
 fetchResults(target)
 
 
 
 
-function getDayName(number){
-    switch (number){
-        case 0: 
-            return 'Sunday'
-        case 1: 
-            return 'Monday'
-        case 2: 
-            return 'Tuesday'
-        case 3: 
-            return 'Wednesday'
-        case 4: 
-            return 'Thursday'
-        case 5: 
-            return 'Friday'
-        case 6: 
-            return 'Saturday'
+function updateDetails(locationName, time, temp, condition){
+    let splitDate = time.split(' ')[0]
+    let splitTime = time.split(' ')[1]
+    let currentDay = getDayName(new Date(splitDate).getDay())      
+    // new Date() — это конструктор встроенного объекта Date
+
+    locationField.innerHTML = locationName
+    dateandTimeField.innerHTML = `${splitDate} ${splitTime} ${currentDay}`
+    temperatureField.innerHTML = temp
+    conditionField.innerHTML = condition
+}
+
+
+function getDayName(dayNumber){
+    switch(dayNumber){
+        case 0: return 'Sunday'
+        case 1: return 'Monday'
+        case 2: return 'Tuesday'
+        case 3: return 'Wednesday'
+        case 4: return 'Thursday'
+        case 5: return 'Friday'
+        case 6: return 'Saturday'
     }
 }
