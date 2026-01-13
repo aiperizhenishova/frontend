b2786011b1ba41588b0e2f245fce409e0e5bbeb2
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2026-01-13 17:11:52 +0600

    created loader

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,11 +1,10 @@
 // http://api.weatherapi.com/v1/current.json?key=f2c2286185c44da9b33115026260901&q=Bishkek&aqi=no
 
-
-const temperatureField = document.querySelector(".temperature");
-const locationField = document.querySelector(".time-location p");
-const dateandTimeField = document.getElementById(".time-location span");
-const conditionField = document.querySelector(".condition p");
-const form = document.querySelector(".form");
+const temperatureField = document.querySelector(".temperature")
+const locationField = document.querySelector(".time-location p")
+const dateandTimeField = document.querySelector(".time-location span")
+const conditionField = document.querySelector(".condition")
+const form = document.querySelector(".form")
 const searchField = document.querySelector(".search-area")
 
 
@@ -14,11 +13,9 @@ form.addEventListener('submit', searchForLocation)
 let target = 'Karakol'
 
 
-const fetchResults = async (targetLocation)=>{
-    let url = `http://api.weatherapi.com/v1/current.json?key=f2c2286185c44da9b33115026260901&q=${targetLocation}&aqi=no`
+const fetchResults = async (target)=>{
+    let url = `http://api.weatherapi.com/v1/current.json?key=f2c2286185c44da9b33115026260901&q=${target}&aqi=no`
 
-    // fetch(url) — это функция и она не возвращает сразу данные, а возвращает Promise — это как обещание:
-    // «Я принесу данные позже, когда сервер ответит» 
     let res = await fetch(url)
     let data = await res.json()
 
@@ -38,7 +35,7 @@ function searchForLocation(e){
 
     target = searchField.value 
 
-    fetchResults(target) 
+    fetchResults(target)
 }
 fetchResults(target)
 
@@ -47,16 +44,19 @@ fetchResults(target)
 
 function updateDetails(locationName, time, temp, condition){
     let splitDate = time.split(' ')[0]
-    let splitTime = time.split(' ')[1]
-    let currentDay = getDayName(new Date(splitDate).getDay())      
-    // new Date() — это конструктор встроенного объекта Date
+    let parts = splitDate.split('-')
+    let year = parts[0]
+    let day = parts[1]
+    // let splitTime = time.split(' ')[1]
+    let currentDay = getDayName(new Date(splitDate).getDay())
+    let currentMonth = getMonthName(new Date(splitDate).getMonth())
 
-    locationField.innerHTML = locationName
-    dateandTimeField.innerHTML = `${splitDate} ${splitTime} ${currentDay}`
     temperatureField.innerHTML = temp
+    locationField.innerHTML = locationName
+    dateandTimeField.innerHTML = `${year} ${day} ${currentMonth} ${currentDay} `
     conditionField.innerHTML = condition
-}
 
+}
 
 function getDayName(dayNumber){
     switch(dayNumber){
@@ -67,5 +67,24 @@ function getDayName(dayNumber){
         case 4: return 'Thursday'
         case 5: return 'Friday'
         case 6: return 'Saturday'
+        default: return 'Unknown'
+    }
+}
+
+function getMonthName(monthNumber){
+    switch(monthNumber){
+        case 0: return 'January'
+        case 1: return 'February'
+        case 2: return 'March'
+        case 3: return 'April'
+        case 4: return 'May'
+        case 5: return 'June'
+        case 6: return 'July'
+        case 7: return 'August'
+        case 8: return 'September'
+        case 9: return 'Oktober'
+        case 10: return  'November'
+        case 11: return  'December'
+        default: return 'Unknown'
     }
 }
